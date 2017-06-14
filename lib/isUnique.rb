1ab441cf58c4b4f9332b0ac6863a492bf85c524f
LauraKirby <laurakirby26@gmail.com>
2017-06-14 16:13:12 -0700

    optimize isUnique by setting the value of result instead of having to do another hash lookup, return from method as soon as we see a repeating letter

--- lib/isUnique.rb
+++ lib/isUnique.rb
@@ -1,23 +1,31 @@
 def isUnique(str)
   hash_map = {}
+  result = true
   str.each_char do |c|
     if hash_map[c] && hash_map[c] > 0
+      result = false
       hash_map[c] += 1
+      result = false
+      puts "result: #{result}"
+      return
     else
       hash_map[c] = 1
     end
   end
-  result = hash_map.has_value?(2)
-  puts result ? "false" : "true"
+  puts "result: #{result}"
 end
 
-isUnique("hi")
 puts "hi - should be true"
-isUnique("hello")
-puts "hello - should be false"
+isUnique("hi")
+puts "\n\n"
 
-isUnique("helol")
+puts "hello - should be false"
+isUnique("hello")
+puts "\n\n"
+
 puts "helol - should be false"
+isUnique("helol")
+puts "\n\n"
 
 # test cases:
 # hi
@@ -31,3 +39,6 @@ puts "helol - should be false"
 
 # an optimization might be to check if string length is 1,
 # then return true
+
+# optimization:
+# return false as soon as we set a key's value to "2"
