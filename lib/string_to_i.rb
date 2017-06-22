6344d4389bfcf5a258a6a9c32c80ddc3f6368727
LauraKirby <laurakirby26@gmail.com>
2017-06-21 22:09:33 -0700

    practicing tdd for 'string_to_i' method

---
+++ lib/string_to_i.rb
@@ -0,0 +1,16 @@
+# Implement atoi to convert a string to an integer.
+
+# Hint: Carefully consider all possible input cases.
+
+# @param {String} str
+# @return {Integer}
+
+# input cases
+
+def my_atoi(str)
+  if str.class == Integer
+    return str
+  end
+  # str_arr = str.split("")
+  # str_arr.each
+end
---
+++ spec/string_to_i_spec.rb
@@ -0,0 +1,39 @@
+require '_spec_helper'
+
+describe "#string_to_i" do
+  it "recieves an integer and returns that integer" do
+    expect(my_atoi(12)).to eq 12
+  end
+
+  # String
+  # space character: " "
+  # => 0
+
+  # String with one int value:
+  # "1"
+  # => 1
+
+  # String with one int value followed by a char:
+  # "1a"
+  # => 1
+
+  # String that begins with a char and ends with an int
+  # "a1"
+  # => 1
+
+  # String that has a char and a space before the int
+  # "a 1"
+  # => 1
+
+  # String that has a symbol before the int
+  # "%1"
+  # => 1
+
+  # String that has 2 integers in a row
+  # "12"
+  # => 12
+
+  # String that has 2 integers with chars on either side
+  # "a1b2c"
+  # "12"
+end
