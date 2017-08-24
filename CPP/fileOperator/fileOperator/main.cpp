50cf7e21b520845f6a9412fadc3720c52611d10e
LauraKirby <laurakirby26@gmail.com>
2017-08-23 17:33:44 -0700

    add working version of Julie's source code. given updated version of C++ and Standford's updated C++ library (see notes at beginning of main.cpp for setup information).

--- CPP/fileOperator/fileOperator/main.cpp
+++ CPP/fileOperator/fileOperator/main.cpp
@@ -5,6 +5,17 @@
 //  Created by Laura Kirby on 8/22/17.
 //  Copyright © 2017 LK. All rights reserved.
 //
+//  The source code below is an updated version material presented in
+//  Stanford CS106B Programming Abstractions Course (2008), Lecture 6
+//  by Julie Zelenski
+//  C++ version: Apple LLVM version 8.1.0 (clang-802.0.42)
+//  Sources:
+//    Youtube:
+//      - https://www.youtube.com/watch?v=eYTDzvlAZMM&index=6&list=PLFE6E58F856038C69
+//    Stanford public course:
+//      - https://see.stanford.edu/Course/CS106B
+//      - https://see.stanford.edu/materials/icspacs106b/Lecture06.pdf
+//      - https://see.stanford.edu/materials/icspacs106b/Lecture6code.txt
 
 #include <fstream>
 #include <iostream>
@@ -16,6 +27,9 @@
 #include "simpio.h"
 #include "strlib.h"
 #include "filelib.h"
+#include "set.h"
+#include "random.h"
+#include <unistd.h>
 using namespace std;
 
 void ReadFile(ifstream &in, Map<string, int> &m)
@@ -31,15 +45,59 @@ void ReadFile(ifstream &in, Map<string, int> &m)
             m[word]++;
         else
             m[word] = 1;
-
     }
     cout << "num unique words " << m.size() << endl;
+}
+
+
+void ReadFileTwo(ifstream &in, Map<string, int> &mapTwo)
+{
+    while (true) {
+        string word;
+        in >> word;
+        if (in.fail()) break;
+        if (mapTwo.containsKey(word))
+            mapTwo[word]++;
+        else
+            mapTwo[word] = 1;
+    }
+    Map<string, int>::iterator itr = mapTwo.begin();
+    string max;
+    int maxCount = 0;
+    while (itr != mapTwo.end()) {
+        string key = *itr;
+        if (mapTwo[key] > maxCount) {
+            max = key;
+            maxCount = mapTwo[key];
+        }
+        ++itr;
+    }
+    cout << "Max is '" << max << "' = " << maxCount << endl;
+}
+
+void TestRandom()
+{
+    Set<int> seen;
+    while (true) {
+        int num = randomInteger(1, 100);
+        if (seen.contains(num)) break;
+        seen.add(num);
+    }
+    Set<int>::iterator itr = seen.begin();
+    while (itr != seen.end()){
+        cout << "number seen: " << *itr << endl;
+        ++itr;
+    }
 }
 
 int main()
 {
     Map<string, int> counts;
-    ifstream in ("example.txt");
+    ifstream in ("example2.txt");
     ReadFile(in, counts);
+    ReadFileTwo(in, counts);
+    TestRandom();
+    char * dir = getcwd(NULL, 0); // Platform-dependent, see reference link below
+    printf("Current dir: %s", dir);
     return 0;
 }
